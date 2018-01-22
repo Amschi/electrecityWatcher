b3371c91bab4c2c4315067e79d19d3092e628a84
Amaury SCHILLIO <asc@ppcmbp1728-asc.local>
2018-01-22 16:39:58 +0100

    add thymeleaf, loggin page, front layout added

--- src/main/java/web/HomeController.java
+++ src/main/java/web/HomeController.java
@@ -1,15 +1,15 @@
-package Web;
+package web;
 
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
 public class HelloController {
 
-  @RequestMapping("/")
-  public String index() {
-    return "Greetings from Spring Boot!";
+  public String loginForm() {
+    return "hello";
   }
 
 }
+
